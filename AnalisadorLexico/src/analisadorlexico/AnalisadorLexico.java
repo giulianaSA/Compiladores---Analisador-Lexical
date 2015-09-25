abdcd21b5aad1624b98efd9b06f93b33b59d2b8d
ThFAlves <thiago101194@hotmail.com>
2015-09-25 10:23:37 -0300

    alteracao

--- AnalisadorLexico/src/analisadorlexico/AnalisadorLexico.java
+++ AnalisadorLexico/src/analisadorlexico/AnalisadorLexico.java
@@ -103,7 +103,6 @@ public class AnalisadorLexico {
             System.out.println(lista1);
         }
 
-        System.out.println("Total de linhas = "+cont);
     }
 
     public static void pegaToken(char letra) {
